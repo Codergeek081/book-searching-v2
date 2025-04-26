207975739b5df7efc21d06937dd56873b12a5a6f
Codergeek081 <issefarah15@gmail.com>
2025-04-26 11:23:44 -0400

    Update auth.ts

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -12,24 +12,21 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
-
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-
-    const secretKey = process.env.JWT_SECRET_KEY || '';
-
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(401); // unauthorized
-      }
-
-      req.user = user as JwtPayload;
-      console.log(req.user)
-      return next();
-    });
+  if (!authHeader) {
+    return next();
   }
 
-  return next();
+  const token = authHeader.split(' ')[1];
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.sendStatus(401);
+    }
+
+    req.user = user as JwtPayload;
+    return next();
+  });
 };
 
 export const signToken = (username: string, email: string, _id: unknown) => {
